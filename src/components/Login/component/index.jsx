1e76e5e5f5e41e866e42ee5656ddab03d5390b97
sara-trueigtech <sakhan@trueigtech.com>
2026-01-26 23:27:09 +0530

    feat: added login form with react-hook-form validation

--- src/components/Login/component/index.jsx
+++ src/components/Login/component/index.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { useForm } from 'react-hook-form';
 
 const Login = () => {
+  const {register, handleSubmit, formState} = useForm();
+
   return (
-    <div>Login</div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input type='email' {...register("email", {required:"email is required"})} placeholder='email'/>
+        {formState.errors.email && <p>{formState.errors.email.message}</p>}
+        <input type='password' {...register("password", {required:"password is required"})} placeholder='password'/>
+        {formState.errors.password && <p>{formState.errors.password.message}</p>}
+
+        <button type='submit'>login</button>
+      </form>
+    </>
   )
 }
 
