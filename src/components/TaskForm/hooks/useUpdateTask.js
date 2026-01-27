8065f51aa51f8d51091012e7950dcd4529060c11
sara-trueigtech <sakhan@trueigtech.com>
2026-01-27 10:48:25 +0530

    feat: added custom hook useUpdateTask to update tasks

---
+++ src/components/TaskForm/hooks/useUpdateTask.js
@@ -0,0 +1,15 @@
+import { updateTask } from "../../../services/api"
+
+const useUpdateTask = (setTask) => {
+    const editTask = async(task) => {
+        const updated = await await updateTask(task.id, task);
+
+        setTask((p) => {
+            p.map((t) => (t.id === task.id ? updated : t));
+        });
+    }
+
+    return {editTask};
+}
+
+export default useUpdateTask;
