04e73cebacf043269e9325e31b93636d241e93eb
sara-trueigtech <sakhan@trueigtech.com>
2026-01-27 10:43:37 +0530

    feat: added custom hook useCreateTask to add tasks

---
+++ src/components/TaskForm/hooks/useCreateTask.js
@@ -0,0 +1,11 @@
+import { createTask } from "../../../services/api"
+
+const useCreateTask = (setTask) => {
+    const addTask = async (task) => {
+        const newTask = await createTask(task);
+        setTask((p) => [...p, newTask]);
+    };
+    return {addTask};
+}
+
+export default useCreateTask;
